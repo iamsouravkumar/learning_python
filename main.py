89d9b4c81be94be4773919f9077d1dc27da172b3
sourav <kumarsourav7012@gmail.com>
2024-09-25 20:21:46 +0530

    python

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import hashlib #built in module
 import pandas #external module
-print('hello world')
-print(10)
-print('sourav')
-print(10*10)
+# print('hello world')
+# print(10)
+# print('sourav')
+# print(10*10)
+
+# single line comment
+print("hey i am \"sourav\" kumar \ni am a \"coder\"") # \n = escape sequence character
+
+# multi line comment
+'''
+hey i am "sourav" kumar 
+i am a "coder"
+'''
+
